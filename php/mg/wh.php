0f2c34a10a97e9d354e732fcb94910a75bae375f
Kishore Kumar <kishorevaishnav@gmail.com>
2017-02-19 01:57:32 -0800

    updated

--- php/mg/wh.php
+++ php/mg/wh.php
@@ -5,11 +5,11 @@
 error_log (print_r($_POST, true));
 
 if ($_POST['domain']=='sandbox416c1023822345578eace13713b59705.mailgun.org' && $_POST['event']=='delivered') {
-	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" "+ $_POST['recipient'] + " --> Message Delivered \"}' https://hooks.slack.com/services/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")
+	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $_POST['recipient']  --> Message Delivered \"}' https://hooks.slack.com/services/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")
 }
 
 if ($_POST['domain']=='sandbox416c1023822345578eace13713b59705.mailgun.org' && $_POST['event']=='opened') {
-	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" "+ $_POST['recipient'] + " --> Message Opened it. \"}' https://hooks.slack.com/services/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")
+	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $_POST['recipient']  --> Message Opened it. \"}' https://hooks.slack.com/services/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")
 }
 
 ?>
