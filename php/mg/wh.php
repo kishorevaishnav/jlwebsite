3399fb29831f6db83321a66229922abffb8a5f6e
Kishore Kumar <kishorevaishnav@gmail.com>
2017-02-19 01:43:39 -0800

    updated

--- php/mg/wh.php
+++ php/mg/wh.php
@@ -1,6 +1,6 @@
 <?php
 // Variables
-$_POST = json_decode(file_get_contents('php://input'), true);
+// $_POST = json_decode(file_get_contents('php://input'), true);
 
 error_log (print_r($_POST, true));
 
