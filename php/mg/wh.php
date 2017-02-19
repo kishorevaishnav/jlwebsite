5ed3c1c6265ef7e66abf77506b31d11cf9f89de0
Kishore Kumar <kishorevaishnav@gmail.com>
2017-02-19 09:21:51 -0800

    fixed error

--- php/mg/wh.php
+++ php/mg/wh.php
@@ -5,11 +5,11 @@
 // error_log (print_r($_POST, true));
 $recp = $_POST['recipient'];
 if (strpos($_ENV['MG_APIURL'], $_POST['domain']) !== false && $_POST['event']=='delivered') {
-	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $recp  --> Message Delivered. \"}' $_ENV['SLACK_WH']");
+	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $recp  --> Message Delivered. \"}' ".$_ENV['SLACK_WH']);
 }
 
 if (strpos($_ENV['MG_APIURL'], $_POST['domain']) !== false && $_POST['event']=='opened') {
-	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $recp  --> Message Opened it. \"}' $_ENV['SLACK_WH']");
+	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $recp  --> Message Opened it. \"}' ".$_ENV['SLACK_WH']);
 }
 
 ?>
