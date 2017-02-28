b83580cdb1364ee12fb872c599bc85b6c57de529
Kishore Kumar <kishorevaishnav@gmail.com>
2017-02-28 15:11:53 -0800

    updated mg.php

--- php/mg/wh.php
+++ php/mg/wh.php
@@ -1,15 +1,17 @@
 <?php
+require('env.php');
+
 // Variables
 // $_POST = json_decode(file_get_contents('php://input'), true);
 
 // error_log (print_r($_POST, true));
 $recp = $_POST['recipient'];
-if (strpos($_ENV['MG_APIURL'], $_POST['domain']) !== false && $_POST['event']=='delivered') {
-	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $recp  --> Message Delivered. \"}' ".$_ENV['SLACK_WH']);
+if (strpos($_ENV['JL_MG_APIURL'], $_POST['domain']) !== false && $_POST['event']=='delivered') {
+	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $recp  --> Message Delivered. \"}' ".$_ENV['JL_SLACK_WH']);
 }
 
-if (strpos($_ENV['MG_APIURL'], $_POST['domain']) !== false && $_POST['event']=='opened') {
-	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $recp  --> Message Opened it. \"}' ".$_ENV['SLACK_WH']);
+if (strpos($_ENV['JL_MG_APIURL'], $_POST['domain']) !== false && $_POST['event']=='opened') {
+	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $recp  --> Message Opened it. \"}' ".$_ENV['JL_SLACK_WH']);
 }
 
 ?>
