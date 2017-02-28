3d708c8b98ed4aa10c9e970d1ce95a58edcf7f11
Kishore Kumar <kishorevaishnav@gmail.com>
2017-02-28 14:07:45 -0800

    updated quote.php

--- php/quote.php
+++ php/quote.php
@@ -36,8 +36,8 @@ EOD;
 	// PHP email sender
 	// mail($to, $subject, $body, $headers);
 
-	system("curl -s --user 'api:".$_ENV['JL_MG_APIKEY']."' ".$_ENV['JL_MG_APIURL']." -F from='".$email."' -F to='".$_ENV['JL_MG_TO']."' -F subject='".$subject."' -F text='".$body."' -F html='<-'");
-	error_log("curl -s --user 'api:".$_ENV['JL_MG_APIKEY']."' ".$_ENV['JL_MG_APIURL']." -F from='".$email."' -F to='".$_ENV['JL_MG_TO']."' -F subject='".$subject."' -F text='".$body."' -F html='<-'");
+	system("curl -s --user 'api:".$_ENV['JL_MG_APIKEY']."' ".$_ENV['JL_MG_APIURL']."/messages -F from='".$email."' -F to='".$_ENV['JL_MG_TO']."' -F subject='".$subject."' -F text='".$body."' -F html='<-'");
+	error_log("curl -s --user 'api:".$_ENV['JL_MG_APIKEY']."' ".$_ENV['JL_MG_APIURL']."/messages -F from='".$email."' -F to='".$_ENV['JL_MG_TO']."' -F subject='".$subject."' -F text='".$body."' -F html='<-'");
 	system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $subject  --> New Lead Mail from JSL. \`\`\` $body \`\`\` \"}' ".$_ENV['JL_SLACK_WH']);
 	error_log("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" $subject  --> New Lead Mail from JSL. \"}' ".$_ENV['JL_SLACK_WH']);
 }
