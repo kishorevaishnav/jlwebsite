b45c1fa3c6427a14c04d242db019c8a96175eaa6
Kishore Kumar <kishorevaishnav@gmail.com>
2017-03-02 14:45:23 -0800

    testing apigee

--- js/custom.js
+++ js/custom.js
@@ -133,13 +133,23 @@ $('.mp-singleimg').magnificPopup({
     type: 'image'
 });
 
-$('.mp-gallery').magnificPopup({
-    type: 'image',
-    gallery:{enabled:true},
-});
+// $('.mp-gallery').magnificPopup({
+//     type: 'image',
+//     gallery:{enabled:true},
+// });
 
 $('.mp-iframe').magnificPopup({
     type: 'iframe'
+});
+
+$('.mp-gallery').magnificPopup({
+    type: 'ajax',
+    alignTop: true,
+    overflowY: 'scroll' // as we know that popup content is tall we set scroll overflow by default to avoid jump
+});
+
+$('.simple-ajax-popup').magnificPopup({
+    type: 'ajax'
 });
 
 
